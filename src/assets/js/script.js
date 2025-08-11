0822be8d3d48a1851f27f31bf6d4b41c8b54e323
DevelopingJeremy <quesadajeremy7@gmail.com>
2025-08-11 10:35:24 -0600

    Se añaden animaciones de projects, se finaliza toda la informacion, listo para Technologies

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -25,4 +25,32 @@ document.addEventListener('DOMContentLoaded', () => {
         closer.classList.toggle('closer-close');
         closer.classList.toggle('closer-open');
     });
+
+    // Place images on the left or right depending on the child element
+    const projects = document.getElementsByClassName('project-card');
+    
+    Array.from(projects).forEach((card, index) => {
+        if (index % 2 == 1 && window.innerWidth > 1000) {
+            card.style.flexDirection = 'row-reverse'
+            card.classList.add('from-left');
+        } else {
+            card.classList.add('from-right');
+        }
+    })
+
+    const projectCards = document.querySelectorAll('.project-card');
+
+    // Intersection Observer to detect when it appears on the monitor
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.2
+    });
+
+    projectCards.forEach(card => observer.observe(card));
 });
